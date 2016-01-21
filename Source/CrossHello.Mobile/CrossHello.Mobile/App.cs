6d394efaf21b981b89659e99fe823847817e257f
Wojciech Witek <wojtekwitek@gmail>
2016-01-21 04:15:24 +0100

    Separate PCL for viewmodels

--- Source/CrossHello.Mobile/CrossHello.Mobile/App.cs
+++ Source/CrossHello.Mobile/CrossHello.Mobile/App.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using CrossHello.Mobile.ViewModels;
 using CrossHello.Mobile.Views;
 using Xamarin.Forms;
 
---
+++ Source/CrossHello.ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,12 @@
+﻿namespace CrossHello.ViewModels
+{
+    public class MainWindowViewModel
+    {
+        public string SayHiText { get; set; }
+
+        public MainWindowViewModel()
+        {
+            SayHiText = "Welcom from the PCL side ;)";
+        }
+    }
+}
